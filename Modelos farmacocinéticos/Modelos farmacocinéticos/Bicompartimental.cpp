a21afb29531d8828d6ae63a6fd7f219a7888b2c0
gtugablue <gtugablue@gmail.com>
2014-12-19 16:50:32 +0000

    Mais cenas

---
+++ Modelos farmacocinéticos/Modelos farmacocinéticos/Bicompartimental.cpp
@@ -0,0 +1,12 @@
+#include "Bicompartimental.h"
+
+Bicompartimental::Bicompartimental(const double Ke, const double Vap, double tMax, const Dosagem &D):
+Ket(Ke / Vap), Ka()
+{
+
+}
+
+const double Bicompartimental::fKa(const double tMax, const double Ke)
+{
+
+}
--- Modelos farmacocinéticos/Modelos farmacocinéticos/Bicompartimental.h
+++ Modelos farmacocinéticos/Modelos farmacocinéticos/Bicompartimental.h
@@ -1,6 +1,17 @@
 #ifndef BICOMPARTIMENTAL_H
 #define BICOMPARTIMENTAL_H
 
+#include "Dosagem.h"
 
+class Bicompartimental
+{
+private:
+	static const double fKa(const double tMax, const double Ke);
+	const double Ket;
+	const double Ka;
+	const Dosagem D;
+public:
+	Bicompartimental(const double Ket, const double Vap, double tMax, const Dosagem &D);
+};
 
 #endif
