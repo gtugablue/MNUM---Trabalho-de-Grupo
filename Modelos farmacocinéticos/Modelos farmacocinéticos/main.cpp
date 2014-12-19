10e95add37696531fbdba90834b1ab51300926d6
gtugablue <gtugablue@gmail.com>
2014-12-19 14:57:33 +0000

    Casts para double substituidos por 'f'

--- Modelos farmacocinéticos/Modelos farmacocinéticos/main.cpp
+++ Modelos farmacocinéticos/Modelos farmacocinéticos/main.cpp
@@ -3,9 +3,9 @@
 
 #define HORAS_POR_DIA			24
 #define MINUTOS_POR_HORA		60
-#define DOSE_DIARIA				500
-#define NUM_HORAS_2A_TOMA		5 // Entre 4 e 6
-#define VOL_APAR_PLASMA			3920
+#define DOSE_DIARIA				500f
+#define NUM_HORAS_2A_TOMA		5f // Entre 4 e 6
+#define VOL_APAR_PLASMA			3920f
 #define CONST_CIN_ELIM_TOTAL	0.1155
 
 using namespace std;
@@ -16,11 +16,11 @@ double D(double t)
 
 	if (t < MINUTOS_POR_HORA)
 	{
-		return (double)100 / MINUTOS_POR_HORA;
+		return 100 / MINUTOS_POR_HORA;
 	}
-	else if (t < ((double)NUM_HORAS_2A_TOMA + 1) * 60)
+	else if (t < (NUM_HORAS_2A_TOMA + 1) * 60)
 	{
-		return ((double)DOSE_DIARIA - 100) / ((double)NUM_HORAS_2A_TOMA * MINUTOS_POR_HORA);
+		return (DOSE_DIARIA - 100) / (NUM_HORAS_2A_TOMA * MINUTOS_POR_HORA);
 	}
 
 	return 0;
