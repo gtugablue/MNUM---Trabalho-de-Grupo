6f06203dbc91deff1c64e6f15b506ef98122fb96
gtugablue <gtugablue@gmail.com>
2014-12-20 20:11:39 +0000

    Equações Diferenciais updated

--- Modelos farmacocinéticos/Modelos farmacocinéticos/EquacoesDiferenciais.cpp
+++ Modelos farmacocinéticos/Modelos farmacocinéticos/EquacoesDiferenciais.cpp
@@ -1,41 +1,43 @@
 #include "EquacoesDiferenciais.h"
 #include <cmath>
 
-double metodoEuler(double(*f)(double x, double y), double xi, double xf, double y, double h)
+double metodoEuler(double f(double, double), double x, double xf, double y, int num_steps)
 {
-	unsigned n = (xf - xi) / h;
-	double newX = xi;
-	for (size_t i = 0; i <= n; ++i)
+	double h = (xf - x) / num_steps;
+
+	for (int i = 0; i < num_steps; ++i)
 	{
-		y += h * f(newX, y);
-		newX += h;
+		y += h * f(x, y);
+		x += h;
 	}
-	return pow(xi, 2) + pow(y, 2);
+
+	return y;
 }
 
-double metodoRungaKutta2a(double f(double x, double y), double xi, double xf, double y, double h)
+double metodoRungaKutta2a(double f(double, double), double x, double xf, double y, int num_steps)
 {
-	unsigned n = (xf - xi) / h;
-	double newX = xi;
-	for (size_t i = 0; i <= n; ++i)
+	double h = (xf - x) / num_steps;
+
+	for (int i = 0; i < num_steps; ++i)
 	{
-		y += h * f(newX + h / 2, y + h / 2 * f(newX, y));
-		newX += h;
+		y += h * f(x + h / 2, y + h / 2 * f(x, y));
+		x += h;
 	}
 	return y;
 }
 
-double metodoRungaKutta4a(double f(double x, double y), double xi, double xf, double y, double h)
+double metodoRungaKutta4a(double f(double, double), double x, double xf, double y, int num_steps)
 {
-	unsigned n = (xf - xi) / h;
+	double h = (xf - x) / num_steps;
 	double deltaY1, deltaY2, deltaY3, deltaY4;
-	for (size_t i = 0; i <= n; ++i)
+	for (int i = 0; i < num_steps; ++i)
 	{
-		deltaY1 = h * f(xi, y);
-		deltaY2 = h * f(xi + h / 2, y + deltaY1 / 2);
-		deltaY3 = h * f(xi + h / 2, y + deltaY2 / 2);
-		deltaY4 = h * f(xi + h, y + deltaY3);
-		xi += h;
+		deltaY1 = h * f(x, y);
+		deltaY2 = h * f(x + h / 2, y + deltaY1 / 2);
+		deltaY3 = h * f(x + h / 2, y + deltaY2 / 2);
+		deltaY4 = h * f(x + h, y + deltaY3);
+
+		x += h;
 		y += (1.0 / 6) * deltaY1 + (1.0 / 3) * deltaY2 + (1.0 / 3) * deltaY3 + (1.0 / 6) * deltaY4;
 	}
 	return y;
--- Modelos farmacocinéticos/Modelos farmacocinéticos/EquacoesDiferenciais.h
+++ Modelos farmacocinéticos/Modelos farmacocinéticos/EquacoesDiferenciais.h
@@ -1,6 +1,8 @@
 #ifndef EQUACOES_DIFERENCIAIS_H
 #define EQUACOES_DIFERENCIAIS_H
 
-
+double metodoEuler(double f(double, double), double x, double xf, double y, int num_steps);
+double metodoRungaKutta2a(double f(double, double), double x, double xf, double y, int num_steps);
+double metodoRungaKutta4a(double f(double, double), double x, double xf, double y, int num_steps);
 
 #endif
